01adf08593784d8172902e022705d256bafca29e
aditya-gaikwad-1233 <adityagaikwad9119@gmail.com>
2023-10-01 14:52:26 +0530

    added hover effects to footer section and aligned it to centre

--- src/Components/Common/Footer.jsx
+++ src/Components/Common/Footer.jsx
@@ -5,12 +5,12 @@ const Footer = () => {
   return (
         <footer>
           <div className="Footer_Social center-div">
-            <Link to="https://www.facebook.com/pccoerpune/" className="icon-link" > <i className="fa-brands fa-facebook"></i></Link>
-            <Link to="https://www.linkedin.com/company/pccoer-pcet/" className="icon-link" > <i className="fa-brands fa-linkedin"></i></Link>
-            <Link to = "https://www.instagram.com/pccoerpcet/" className="icon-link" > <i className="fa-brands fa-instagram"></i></Link>
-            <Link to="https://github.com/shreeramshanmugasundaram/gdsc-website" className="icon-link" > <i className="fa-brands fa-github"></i></Link>
-            <Link to="https://www.youtube.com/channel/UChJFHByu-xP4ti5O-HSKvFA" className="icon-link" > <i className="fa-brands fa-youtube"></i></Link>
-            <Link to="mailto:pccoer.ravet@gmail.com?subject=Write Your Message" className="icon-link" > <i className="fa-solid fa-envelope" ></i></Link>
+            <Link to="https://www.facebook.com/pccoerpune/" className="icon-link facebook" > <i className="fa-brands fa-facebook"></i></Link>
+            <Link to="https://www.linkedin.com/company/pccoer-pcet/" className="icon-link linkedin" > <i className="fa-brands fa-linkedin"></i></Link>
+            <Link to = "https://www.instagram.com/pccoerpcet/" className="icon-link instagram" > <i className="fa-brands fa-instagram"></i></Link>
+            <Link to="https://github.com/shreeramshanmugasundaram/gdsc-website" className="icon-link github" > <i className="fa-brands fa-github"></i></Link>
+            <Link to="https://www.youtube.com/channel/UChJFHByu-xP4ti5O-HSKvFA" className="icon-link youtube" > <i className="fa-brands fa-youtube"></i></Link>
+            <Link to="mailto:pccoer.ravet@gmail.com?subject=Write Your Message" className="icon-link envelope" > <i className="fa-solid fa-envelope" ></i></Link>
           </div>
     </footer>
   )
